d3ee92edf77034769f938454d813ad37e56a4eff
Satyam Saxena <satyam2saxena@gmail.com>
2021-01-18 18:17:51 +0530

    Fix Bug in Forecast component

--- src/Components/Forecast/Forecast.js
+++ src/Components/Forecast/Forecast.js
@@ -1,19 +1,17 @@
 import {useState} from 'react';
 const Forecast = ()=>{
-    let [res,setResponse] = useState();
+    let [res,setResponse] = useState({});
     const getForecast = ()=>{
-        fetch("https://community-open-weather-map.p.rapidapi.com/weather?q=Firozabad", {
+        fetch("https://community-open-weather-map.p.rapidapi.com/weather?q=Seattle", {
             "method": "GET",
             "headers": {
-                "x-rapidapi-key": "15da8dc131msh798f7df504566b1p1295f8jsnc548d0e30281",
+                "x-rapidapi-key": "9f9bcdd758msh8f45bd9580c94a0p1e50bajsnc53f1c6d1416",
                 "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
             }
         })
-        .then(response=>{
-            response = response.json();
-            setResponse((response)=>{
-                res = response;
-            })
+        .then(response=>response.json())
+        .then(res=>{
+            setResponse(res)
         })
         .catch(err => {
             console.error(err);
@@ -24,7 +22,7 @@ const Forecast = ()=>{
         <>
             <button onClick={getForecast()}>Get Forecast</button>
             <div>
-                {JSON.stringify(res)}
+                <p>{JSON.stringify(res)}</p>
             </div>
 
         </>
