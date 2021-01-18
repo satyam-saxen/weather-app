73d281d807c71ada574a8046195f99b93a4bc3d2
Satyam Saxena <satyam2saxena@gmail.com>
2021-01-18 17:19:03 +0530

    Fetching weather through API in getForecast function

--- src/Components/Forecast/Forecast.js
+++ src/Components/Forecast/Forecast.js
@@ -1,9 +1,34 @@
 import {useState} from 'react';
 const Forecast = ()=>{
-    const [res,setResponse] = useState();
-    getForecast(){
+    let [res,setResponse] = useState();
+    const getForecast = ()=>{
+        fetch("https://community-open-weather-map.p.rapidapi.com/weather?q=Firozabad", {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-key": "15da8dc131msh798f7df504566b1p1295f8jsnc548d0e30281",
+                "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
+            }
+        })
+        .then(response=>{
+            response = response.json();
+            setResponse((response)=>{
+                res = response;
+            })
+        })
+        .catch(err => {
+            console.error(err);
+        });
+    }
 
-    }.then(respo)
+    return(
+        <>
+            <button onClick={getForecast()}>Get Forecast</button>
+            <div>
+                {JSON.stringify(res)}
+            </div>
+
+        </>
+    )
 
 
 }
