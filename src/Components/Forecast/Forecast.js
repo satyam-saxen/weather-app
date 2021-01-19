2619d230b4863e1d7d9613653701076ffca1aec1
Satyam Saxena <satyam2saxena@gmail.com>
2021-01-19 11:03:15 +0530

    Worked API

--- src/Components/Forecast/Forecast.js
+++ src/Components/Forecast/Forecast.js
@@ -9,36 +9,24 @@ const Forecast = ()=>{
 
 
     function getForecast(e){
-        // fetch(`https://community-open-weather-map.p.rapidapi.com/weather?q=${uriEncodedCity}&units=${units}`, {
-        //     "method": "GET",
-        //     "headers": {
-        //         "x-rapidapi-key": "9f9bcdd758msh8f45bd9580c94a0p1e50bajsnc53f1c6d1416",
-        //         "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
-        //     }
-        // })
-        // .then(response =>{ 
-        //     console.log(response);
-        //     response.json();
-        // })
-        // .then(setResponseObj((response)=>{
-        //     responseObj = response;
-        // }))
-        // .catch(err => {
-        //     console.error(err);
-        // });
-        console.log(`Units = ${units}`);
-        console.log(`City = ${uriEncodedCity}`);
+        e.preventDefault();
+        fetch(`https://community-open-weather-map.p.rapidapi.com/weather?q=${uriEncodedCity}&units=${units}`, {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-key": "9f9bcdd758msh8f45bd9580c94a0p1e50bajsnc53f1c6d1416",
+                "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
+            }
+        })
+        .then(response =>response.json())
+        .then(response=>setResponseObj(response))
+        .catch(err => {
+            console.error(err);
+        })
         
     }
-    responseObj = {"coord":{"lon":-122.3321,"lat":47.6062},"weather":[{"id":804,"main":"Clouds",
-        "description":"overcast clouds","icon":"04n"}],"base":"stations","main":{"temp":279.53,
-        "feels_like":277.6,"temp_min":278.15,"temp_max":280.93,"pressure":1035,"humidity":93},
-        "visibility":10000,"wind":{"speed":1.24,"deg":1},"clouds":{"all":90},"dt":1610971805,
-        "sys":{"type":1,"id":5692,"country":"US","sunrise":1610985016,"sunset":1611017346},
-        "timezone":-28800,"id":5809844,"name":"Seattle","cod":200};
     return(
         <div>
-            <form onSubmit={getForecast()}>
+            <form onSubmit={getForecast}>
                 <input
                     id="city"
                     type="text"
--- src/Components/component/Component.js
+++ src/Components/component/Component.js
@@ -1,13 +1,15 @@
 const Component = (props)=>{
     return(
         <>
-            {props.responseObj.cod === 200 ?
-            <div>
-                <h2>City {props.responseObj.name}</h2>
-                <div>Temp: {Math.round(props.responseObj.main.temp)}</div>
-                <div>Humidity: {props.responseObj.main.humidity}</div>
-            </div>
-            : null}
+            {props.responseObj === undefined ? <div>Data</div> : 
+                props.responseObj.cod === 200 ?
+                <div>
+                    <h2>City {props.responseObj.name}</h2>
+                    <div>Temp: {Math.round(props.responseObj.main.temp)}</div>
+                    <div>Humidity: {props.responseObj.main.humidity}</div>
+                </div>
+                : null
+            }
         </>
     );
 }
