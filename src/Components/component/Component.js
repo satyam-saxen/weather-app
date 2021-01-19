d6795b63fb1095a791e7677ee3fa796f0056f12a
Satyam Saxena <satyam2saxena@gmail.com>
2021-01-19 22:14:38 +0530

    Test for response object not to be null

--- src/Components/component/Component.js
+++ src/Components/component/Component.js
@@ -6,13 +6,13 @@ const Component = (props)=>{
             : props.responseObj === undefined ? <div data-testid="undefined-res"></div> : 
                 props.responseObj.cod === 200 ?
                 <div>
-                    <h4>City {props.responseObj.name}</h4>
+                    <h4 data-testid="status200">City {props.responseObj.name}</h4>
                     <div>Temp: {Math.round(props.responseObj.main.temp)}</div>
                     <div>Humidity: {props.responseObj.main.humidity}</div>
                     <div>Pressure: {props.responseObj.main.pressure}</div>
                     <div>Weather: {props.responseObj.weather[0].description}</div>
                 </div>
-                : <div>{props.responseObj.message}</div>
+                : <div data-testid="statusNot200">{props.responseObj.message}</div>
             }
         </div>
     );
--- src/Components/component/__test__/Component.test.js
+++ src/Components/component/__test__/Component.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Component from './../Component';
-import { render } from '@testing-library/react';
+import { render,cleanup } from '@testing-library/react';
 import "@testing-library/jest-dom/extend-expect";
+
+afterEach(cleanup);
 
 it("renders without crashing",()=>{
     const div = document.createElement("div");
@@ -16,6 +18,26 @@ it("checks for the error props to be true",()=>{
 
 
 it("checks for the error props to be false and responseObj undefined",()=>{
-    const {getByTestId} = render(<Component error={false} responseObj={undefined}></Component>);
-    expect(getByTestId('undefined-res')).toHaveLength(0);
+    const responseObj = undefined;
+    const {getByTestId} = render(<Component error={false} responseObj={responseObj}></Component>);
+    expect(getByTestId('undefined-res')).toBeDefined();
+})
+
+
+it("checks for the response to be 200",()=>{
+    const responseObj = {"coord":{"lon":-122.3321,"lat":47.6062},"weather":[{"id":804,"main":"Clouds",
+    "description":"overcast clouds","icon":"04n"}],"base":"stations","main":{"temp":279.53,
+    "feels_like":277.6,"temp_min":278.15,"temp_max":280.93,"pressure":1035,"humidity":93},
+    "visibility":10000,"wind":{"speed":1.24,"deg":1},"clouds":{"all":90},"dt":1610971805,
+    "sys":{"type":1,"id":5692,"country":"US","sunrise":1610985016,"sunset":1611017346},
+    "timezone":-28800,"id":5809844,"name":"Seattle","cod":200};
+    const {getByTestId} = render(<Component error={false} responseObj={responseObj}></Component>);
+    expect(getByTestId('status200')).toHaveTextContent("City Seattle");
+})
+
+
+it("checks for the response not to be 200",()=>{
+    const responseObj = {"cod":404,"message":"Not Found"};
+    const {getByTestId} = render(<Component error={false} responseObj={responseObj}></Component>);
+    expect(getByTestId('statusNot200')).toHaveTextContent("Not Found");
 })
